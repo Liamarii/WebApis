27b51c77f0bb4d978b5b124cde63ea83ad50e4b0
Liam Costello <liamdcostello@gmail.com>
2025-02-22 13:25:57 +0000

    Added a singleton logger to the users project.

--- Users/Controllers/Users.cs
+++ Users/Controllers/Users.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Users.Infrastructure;
 
 namespace Users.Controllers
 {
@@ -10,6 +11,8 @@ namespace Users.Controllers
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
         public ActionResult<string> Get()
         {
+            Logs.Add.InfoLog("This is an info log");
+            Logs.Add.ErrorLog("This is an error log");
             return Ok("User 1");
         }
     }
---
+++ Users/Infrastructure/Logs.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Users.Infrastructure
+{
+    public sealed class Logs
+    {
+        private static readonly Lazy<Logs> _instance = new(() => new Logs());
+        private readonly ILogger _logger;
+
+        private Logs() {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            _logger = LoggerFactory
+                .Create(x => x.AddDebug())
+                .CreateLogger<Logs>();
+        }
+
+        public static Logs Add => _instance.Value;
+
+        public void InfoLog(string message, [CallerMemberName] string callerName = "", [CallerFilePath] string filePath = "")
+        {
+            Log(LogLevel.Information, message, callerName, filePath);
+        }
+
+        public void WarningLog(string message, [CallerMemberName] string callerName = "", [CallerFilePath] string filePath = "")
+        {
+            Log(LogLevel.Warning, message, callerName, filePath);
+        }
+
+        public void ErrorLog(string message, [CallerMemberName] string callerName = "", [CallerFilePath] string filePath = "")
+        {
+            Log(LogLevel.Error, message, callerName, filePath);
+        }
+
+        public void DebugLog(string message, [CallerMemberName] string callerName = "", [CallerFilePath] string filePath = "")
+        {
+            Log(LogLevel.Debug, message, callerName, filePath);
+        }
+
+        private void Log(LogLevel logLevel, string message, string callerName, string filePath)
+        {
+            _logger.Log(logLevel, "\n {logLevel} of \"{message}\" was logged by the {callerName} method in {filePath} \n", logLevel, message, callerName, filePath);
+        }
+    }
+}
