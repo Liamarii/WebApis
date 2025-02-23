451f8a52334695a87520e7e245e8bcaef7608c4e
Liam Costello <liamdcostello@gmail.com>
2025-02-23 18:45:47 +0000

    Adding some basic xunit vehicle tests

---
+++ VehiclesTests/GetVehicles/GivenARequestToGetVehicles.cs
@@ -0,0 +1,40 @@
+﻿using Vehicles.Features.GetVehicles;
+
+namespace VehiclesTests.GetVehicles
+{
+    public class VehicleTests : IAsyncLifetime
+    {
+        private GetVehiclesRequest? _request;
+        private GetVehiclesResponse? _response;
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task InitializeAsync()
+        {
+            _request = new GetVehiclesRequest() { Make = "Toyota" };
+            var sut = new GetVehiclesHandler();
+            _response = await sut.Handle(_request, CancellationToken.None);
+        }
+
+        [Fact]
+        public void TheResponseIsNotNull()
+        {
+            Assert.NotNull(_response);
+        }
+
+        [Fact]
+        public void TheResponseContainsVehicles()
+        {
+            Assert.NotEmpty(_response!.Vehicles);
+        }
+
+        [Fact]
+        public void TheReponseContainsOnlyToyotaVehicles()
+        {
+            Assert.True(_response!.Vehicles.All(x => x.Make == _request?.Make));
+        }
+    }
+}
