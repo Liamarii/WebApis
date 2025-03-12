28ff21590b632d2bb89aabf1fab8956d2e3c3ba2
Liam Costello <liamdcostello@gmail.com>
2025-03-12 19:48:29 +0000

    basic selenium web driver setup for later.

---
+++ VehiclesTests/System/Selenium/Account/CreateAccountTests.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace VehiclesTests.System.Selenium.Account
+{
+    [Parallelizable]
+    public class CreateAccountTests : IDisposable
+    {
+        private ChromeDriver? _driver = null;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _driver = new();
+            _driver.Navigate().GoToUrl("http://www.google.com");
+        }
+
+        [Test]
+        public void GoToGoogle()
+        {
+            Assert.That(_driver?.Title, Is.EqualTo("Google"));
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();            
+            GC.SuppressFinalize(this);
+        }
+    }
+}
