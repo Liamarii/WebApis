8f44efa32602a392ffa18cdd5293ee4f44f60d86
Liam Costello <liamdcostello@gmail.com>
2025-03-15 10:27:45 +0000

    created a default response type to pass around rather than lots of hardcoded strings.

--- Vehicles/Api/Controllers/VehiclesController.cs
+++ Vehicles/Api/Controllers/VehiclesController.cs
@@ -11,27 +11,29 @@ namespace Vehicles.Api.Controllers;
 [ApiController]
 public class VehiclesController(IMediator mediator) : ControllerBase
 {
+    private const string defaultResponseType = "application/x-protobuf";
+
     [HttpGet("GetVehicles")]
-    [Produces("application/json", "application/x-protobuf")]
+    [Produces("application/json", defaultResponseType)]
     [Consumes("application/json")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
-    public async Task<ActionResult> GetVehicles(CancellationToken cancellationToken, [FromHeader] string accept = "application/x-protobuf")
+    public async Task<ActionResult> GetVehicles(CancellationToken cancellationToken, [FromHeader] string accept = defaultResponseType)
     {
         GetVehiclesResponse response = await mediator.Send(new GetVehiclesRequest(), cancellationToken);
-        return accept == "application/x-protobuf" ?
-            File(ProtobufHelper.SerialiseToProtobuf(response), "application/x-protobuf") :
+        return accept == defaultResponseType ?
+            File(ProtobufHelper.SerialiseToProtobuf(response), defaultResponseType) :
             Ok(response);
     }
 
     [HttpPost("GetVehiclesByMake")]
-    [Produces("application/json", "application/x-protobuf")]
+    [Produces("application/json", defaultResponseType)]
     [Consumes("application/json")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
-    public async Task<ActionResult> GetVehiclesByMake([FromBody, Required] GetVehiclesByMakeRequest getVehiclesByMakeRequest, CancellationToken cancellationToken, [FromHeader] string accept = "application/x-protobuf")
+    public async Task<ActionResult> GetVehiclesByMake([FromBody, Required] GetVehiclesByMakeRequest getVehiclesByMakeRequest, CancellationToken cancellationToken, [FromHeader] string accept = defaultResponseType)
     {
         GetVehiclesByMakeResponse response = await mediator.Send(getVehiclesByMakeRequest, cancellationToken);
-        return accept == "application/x-protobuf" ?
-            File(ProtobufHelper.SerialiseToProtobuf(response), "application/x-protobuf") :
+        return accept == defaultResponseType ?
+            File(ProtobufHelper.SerialiseToProtobuf(response), defaultResponseType) :
             Ok(response);
     }
 }
