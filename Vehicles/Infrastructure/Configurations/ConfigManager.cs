f4aaa7d4f5fdb67da32c516f7176c5fce47ccf76
Liam Costello <liamdcostello@gmail.com>
2025-03-30 18:03:58 +0100

    Clean up the config manager setup in the program file.

---
+++ Vehicles/Infrastructure/Configurations/ConfigManager.cs
@@ -0,0 +1,28 @@
+﻿namespace Vehicles.Infrastructure.Configurations
+{
+    public static class ConfigManager
+    {
+        private static readonly string? _environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        private static readonly Lazy<IConfiguration> _configuration = new(BuildConfiguration);
+
+        private static IConfiguration BuildConfiguration()
+        {
+            if (string.IsNullOrWhiteSpace(_environment))
+            {
+                throw new InvalidOperationException("Environment variable ASPNETCORE_ENVIRONMENT is not set.");
+            }
+
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{_environment}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        public static IServiceCollection AddConfigurationMapping<T>(this IServiceCollection services, string section) where T : class
+        {
+            return services.Configure<T>(_configuration.Value.GetSection(section));
+        }
+    }
+}
--- Vehicles/Program.cs
+++ Vehicles/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.Extensions.Options;
 using System.Reflection;
 using Vehicles.Data.Repositories;
-using Vehicles.Enums;
 using Vehicles.Infrastructure.Configurations;
 using Vehicles.Infrastructure.Persistence;
 
@@ -18,22 +17,7 @@ public class Program
         builder.Services.AddMediatR(x => x.RegisterServicesFromAssembly(typeof(Program).Assembly));
         builder.Services.AddControllers();
         builder.Services.AddSingleton<IVehiclesRepository, VehiclesRepository>();
-
-        string? environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-        bool unknownEnvironment = Enum.TryParse(environment, true, out EnvironmentTypes environmentType) == false;
-        if (unknownEnvironment)
-        {
-            throw new InvalidOperationException($"Unknown environment: {environment}.");
-        }
-
-        IConfiguration config = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
-            .AddEnvironmentVariables()
-            .Build();
-
-        builder.Services.Configure<DatabaseConfig>(config.GetSection("ConnectionStrings"));
+        builder.Services.AddConfigurationMapping<DatabaseConfig>("ConnectionStrings");
         builder.Services.AddSingleton<INHibernateDatabase, NHibernateDatabase>(x => new NHibernateDatabase(x.GetRequiredService<IOptions<DatabaseConfig>>(), Assembly.GetExecutingAssembly()));
 
         var app = builder.Build();
