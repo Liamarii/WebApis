1001c9cbf155b7ddae44d4f9d7f42bc747f81ba1
Liam Costello <liamdcostello@gmail.com>
2025-04-14 21:57:57 +0100

    fix the unit tests.

--- AngularUI/src/app/components/home/home.component.spec.ts
+++ AngularUI/src/app/components/home/home.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UsersService } from '../../services/users/users.service';
 
 import { HomeComponent } from './home.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
-
+  let usersServiceMock: jasmine.SpyObj<UsersService>;
+  
   beforeEach(async () => {
+    usersServiceMock = jasmine.createSpyObj('UsersService', ['getVehicleByUser']);
+
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent, FormsModule, CommonModule],
+      providers: [{ provide: UsersService, useValue: usersServiceMock } ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
--- AngularUI/src/app/services/users/users.service.spec.ts
+++ AngularUI/src/app/services/users/users.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { UsersService } from './users.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClientTesting(), provideHttpClient()] 
+    });
     service = TestBed.inject(UsersService);
+    httpMock = TestBed.inject(HttpTestingController); 
   });
 
   it('should be created', () => {
