e247fe4a2e5a7a36f20b746fc2155a4be0ec4e47
Liam Costello <liamdcostello@gmail.com>
2025-04-27 18:58:20 +0100

    Cleaned up the test database files to make them more focused.

---
+++ VehiclesTests/Database/DatabaseContainerManager.cs
@@ -0,0 +1,43 @@
+﻿using Testcontainers.PostgreSql;
+
+namespace VehiclesTests.Database;
+
+public class DatabaseContainerManager : IAsyncDisposable
+{
+    private readonly PostgreSqlContainer _postgresContainer;
+    private bool _disposed;
+
+    //TODO: Maybe this should be more generic as it's specific to pg currently.
+
+    public DatabaseContainerManager()
+    {
+        _postgresContainer = new PostgreSqlBuilder()
+            .WithImage("postgres:15")
+            .WithDatabase("testdb")
+            .WithUsername("testuser")
+            .WithPassword("testpassword")
+            .WithName("VehicleTests")
+            .Build();
+    }
+
+    public async Task StartAsync() => await _postgresContainer.StartAsync();
+
+    public string GetConnectionString() => _postgresContainer.GetConnectionString();
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        if (_postgresContainer != null)
+        {
+            await _postgresContainer.StopAsync();
+        }
+
+        _disposed = true;
+
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ VehiclesTests/Database/DatabaseSeeder.cs
@@ -0,0 +1,32 @@
+﻿using Npgsql;
+
+namespace VehiclesTests.Database;
+
+public static class DatabaseSeeder
+{
+    private const string TableName = "vehicles";
+
+    private const string CreateVehiclesTableQuery = @$"
+        CREATE TABLE IF NOT EXISTS {TableName} (
+            id SERIAL PRIMARY KEY,
+            make VARCHAR(100),
+            model VARCHAR(100),
+            year INT
+        );";
+
+    private const string InsertVehicleQuery = @$"
+        INSERT INTO {TableName} (make, model, year) 
+        VALUES ('Hyundai', 'Pancake', 2021),('Bmw', 'Jellyfish', 2050);";
+
+    public static async Task SeedAsync(string connectionString)
+    {
+        await using var connection = new NpgsqlConnection(connectionString);
+        await connection.OpenAsync();
+
+        await using var createCmd = new NpgsqlCommand(CreateVehiclesTableQuery, connection);
+        await createCmd.ExecuteNonQueryAsync();
+
+        await using var insertCmd = new NpgsqlCommand(InsertVehicleQuery, connection);
+        await insertCmd.ExecuteNonQueryAsync();
+    }
+}
